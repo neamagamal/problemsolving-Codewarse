5864caa0da81aaf02cb7ed329c5ef9b92c0efc1d
neamagamal <neamagamal1213@gmail.com>
2024-07-08 14:44:27 +0300

    ps

--- Program.cs
+++ Program.cs
@@ -41,4 +41,65 @@ internal class Program
         }
         return false;
     }
+
+    //Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+    public static string EvenOrOdd(int number)
+    {
+        if (number % 2 == 0)
+        {
+            return "Even";
+        }
+        else return "Odd";
+    }
+    /*Complete the solution so that it reverses the string passed into it.
+  'world'  =>  'dlrow'
+  'word'   =>  'drow'*/
+    public static string Solution(string str)
+    {
+        string reversed = "";
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            reversed += str[i];
+        }
+        return reversed;
+    }
+    //Implement a function which convert the given boolean value into its string representation.
+    public static string BooleanToString(bool b)
+    {
+        return b == true ? "True" : "False";
+    }
+    // There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+    public static int Mango(int quantity, int price)
+    {
+        return (quantity - quantity / 3) * price;
+    }
+    //Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+    int quarter_of(int month)
+    {
+        if (month < 4)
+        {
+            return 1;
+        }
+        else if (month < 7)
+        {
+            return 2;
+        }
+        else if (month < 10)
+        {
+            return 3;
+        }
+        else return 4;
+    }
+
+
+
+
+
+
+
+
+
+
 }
+
+
